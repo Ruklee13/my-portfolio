1c81ed80fda79ad9c30ce917775a76ebd7eb031e
Rukudzo Mushunje <102190023+Ruklee13@users.noreply.github.com>
2024-05-21 16:40:55 -0400

    navbar

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+const DasbboardLayout = ({}) => {
+    return(
+        <div className=" border-black border-4 h-full w-full flex flex-col">
+            <Navbar/>
+            <div className=" border-green-900 border-2"></div>
+        
+        </div>
+    );
+}
+
+export default DasbboardLayout
